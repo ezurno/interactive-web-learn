2bb0efc03f3498b71c04e92b0824f3a346a17fe5
Lee-Junmo <108059303+ezurno@users.noreply.github.com>
2024-04-02 17:51:52 +0900

    Feat: svg animation with 404 pages
    1. svg 애니메이션을 통한 404 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path={"/"} element={<Home />} />
-      <Route path={"/not-found"} element={<NotFound />} />
+      <Route path={"/not-found"} element={<NotFoundPage />} />
     </Routes>
   );
 }
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import NotFound from "../assets/NotFound";
+
+export default function NotFoundPage() {
+  return (
+    <div className="w-full h-screen overflow-hidden bg-violet-900">
+      <NotFound />
+    </div>
+  );
+}
